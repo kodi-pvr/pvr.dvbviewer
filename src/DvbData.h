c163f139b5f4ee88660f057486c5993d75eb033d
Alwin Esch <alwin.esch@web.de>
2020-03-30 18:18:49 +0200

    update source license to use SPDX

--- src/DvbData.h
+++ src/DvbData.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2005-2020 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2013-2020 Manuel Mausz
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
+
 #pragma once
 
 #include "KVStore.h"
