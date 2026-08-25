419c4b5bc2b6733b59570e86994d7e183a995f48
Manuel Mausz <manuel@mausz.at>
2026-08-26 00:24:07 +0200

    Revert "test"
    This reverts commit 9366e50723c8e701bf5ef92ccba5ac915d34e1cb.

--- src/StreamReader.cpp
+++ src/StreamReader.cpp
@@ -32,8 +32,8 @@ StreamReader::~StreamReader(void)
 
 bool StreamReader::Start()
 {
-  return m_streamHandle.CURLOpen(ADDON_READ_TRUNCATED | ADDON_READ_NO_CACHE
-      | READ_AUDIO_VIDEO);
+  return m_streamHandle.CURLOpen(ADDON_READ_TRUNCATED | ADDON_READ_CHUNKED
+      | ADDON_READ_NO_CACHE);
 }
 
 ssize_t StreamReader::ReadData(unsigned char *buffer, unsigned int size)
